adae38aa4c4810806d756fb4fcda4932aa43808f
Rafael Ortega Bueno <rorteg@gmail.com>
2017-11-15 06:27:18 -0200

    ++ Test for i18n configuration

---
+++ test/Module/TranslateAdapterTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @see       https://github.com/rorteg/m1devtools for the canonical source repository
+ * @copyright Copyright (c) 2017 Rafael Ortega Bueno. (https://github.com/rorteg)
+ * @license   https://github.com/rorteg/m1devtools/blob/master/LICENSE.md New BSD License
+ */
+
+namespace ROBTest\M1devtools\Module;
+
+use PHPUnit\Framework\TestCase;
+use ROB\M1devtools\Config;
+use ROB\M1devtools\Translator\TranslatorAdapter;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class TranslateAdapterTest extends TestCase
+{
+    /**
+     * @var TranslatorAdapter
+     */
+    private $translator;
+
+    protected function setUp()
+    {
+        Config::setConfig('translator', [
+            'locale' => 'pt_BR',
+            'translation_file_patterns' => [
+                [
+                    'type' => 'phparray',
+                    'base_dir' => __DIR__ . '/i18n',
+                    'pattern' => '%s/messages.php'
+                ]
+            ]
+        ]);
+        $this->translator = Config::getTranslator();
+    }
+
+    /**
+     * @dataProvider    provideFileTranslate
+     */
+    public function testTranslateAdapterCorrectConfiguration($message, $translated)
+    {
+        $translator = $this->translator;
+        $this->assertEquals($translator->translate($message), $translated);
+    }
+
+    public function provideFileTranslate()
+    {
+        $translate = require __DIR__ . '/i18n/pt_BR/messages.php';
+        $data = [];
+
+        foreach ($translate as $message => $translated) {
+            $data[] = [$message, $translated];
+        }
+
+        return $data;
+    }
+}
---
+++ test/Module/i18n/pt_BR/messages.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @see       https://github.com/rorteg/m1devtools for the canonical source repository
+ * @copyright Copyright (c) 2017 Rafael Ortega Bueno. (https://github.com/rorteg)
+ * @license   https://github.com/rorteg/m1devtools/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * EN-Revision: 15.Nov.2017
+ */
+
+return [
+    'Unit test for confirmation that the translation is working.' =>
+        'Teste unitário para confirmação que a tradução está funcionando.',
+];
